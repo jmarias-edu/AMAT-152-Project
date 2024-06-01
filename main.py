40be5210ac095588b42903c478608c9ad37931ba
jmarias-edu <jmarias@up.edu.ph>
2024-06-01 13:18:07 +0800

    feat: started SIR Model

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+def SIRmodel(S, I, R, contact_rate, recovery_rate):
+    new_infected = contact_rate * S * I
+    new_recover = recovery_rate * I
+
+    new_S = S - new_infected
+    new_I = I + new_infected - new_recoveries
+    new_R = R + new_recoveries
+
+    return new_S, new_I, new_R
+
 def printMenu():
+    print("[1] SIR Model")
     print("[0] Exit")
 
 def getChoice(prompt):
-    return int(input(prompt))
+    while(True):
+        try:
+            return int(input(prompt))
+        except:
+            print("Invalid input, try again")
 
 def main():
     print("Hello World!")
